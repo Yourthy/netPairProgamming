600d450da81d3831399e6a1fbd5c6ebc36e6dce8
Yourthy <christopher.t.yourth@gmail.com>
2020-12-03 19:29:00 +0000

    word search file

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,19 @@
+//Worked with Stewart McKinlay as a peer progamming activity
+
 const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+    const vertJoin = [];
+    for(let rows = 0; rows < letters.length; rows++){
+        let tempString = "";
+        for(let columns = 0; columns <= letters[rows].length; columns++){
+            tempString += letters[columns][rows];
+        }
+        vertJoin.push(tempString);
+    }
+    if(horizontalJoin.includes(word) || vertJoin.includes(word)){
+        return true;
+    } else {
+        return false;
     }
 }
-
-module.exports = wordSearch
+module.exports = wordSearch;
